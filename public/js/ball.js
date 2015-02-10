bf5e03bbf0d2b76c28a08009eb886e030b0f15e3
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-10 16:45:35 +0000

    Pig moving around board

--- public/js/ball.js
+++ public/js/ball.js
@@ -1,16 +1,19 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
-  game.load.image('flyer', 'http://img1.wikia.nocookie.net/__cb20121103175717/angrybirds/images/archive/6/62/20121103180636!Zombie_Pig.png');
+  game.load.image('flyer', 'image/zombiepig.jpg');
 };
 
 var image;
 
 function create() {
 
+  
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   image = game.add.sprite(0, 0, 'flyer');
+  
+  image.scale.setTo(0.08, 0.08);
 
   game.physics.enable(image, Phaser.Physics.ARCADE);
 
