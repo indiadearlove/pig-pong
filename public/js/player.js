f7dda193ae403511ed88ec491330fea133be2ac6
matteomanzo <imatteo.manzo@gmail.com>
2015-02-10 15:23:58 +0000

    image on screen!

--- public/js/player.js
+++ public/js/player.js
@@ -1,15 +1,15 @@
 
-var player = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var player = new Phaser.Game(800, 600, Phaser.AUTO, 'farmer', { preload: preload, create: create });
 
-function load() {
+function preload() {
 
-    player.load.image('mushroom', 'assets/sprites/mushroom2.png');
+    player.load.image('farmer', 'images/farmer.jpg');
 
 }
 
 function create() {
 
-    //  This simply creates a sprite using the mushroom image we loaded above and positions it at 200 x 200
-    var test = player.add.sprite(200, 200, 'mushroom');
-
+    var character = player.add.sprite(player.world.centerX-30, player.world.centerY-30, 'farmer');
+    character.scale.setTo(0.3, 0.3);
+    // character.anchor.
 }
