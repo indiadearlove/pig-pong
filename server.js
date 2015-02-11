c826db2efe50200ccc66b49b08358644a4553bd7
Hannah Carney <hannahcarneyart@gmail.com>
2015-02-11 11:52:23 +0000

    posted username

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 var server = require('http').createServer(app);
 var port = 9999;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response){
   response.render('index');
 });
 
-server.listen(port, function(){
-  console.log('Matteo is a fat prick on port ' + port);
+app.post('/game', function(req, res) {
+  res.send(req.body.username);
 });
+
+server.listen(port, function(){
+  console.log('Server running at' + port);
+});
+
 
 module.exports = server;
