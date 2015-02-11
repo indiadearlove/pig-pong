e65972559b3234260b3d3a44c58fd8e0762e15c9
matteomanzo <imatteo.manzo@gmail.com>
2015-02-11 12:39:37 +0000

    refactored

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,6 @@
-var game = new Phaser.Game(1400, 900, Phaser.AUTO, 'game-mainpage', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1400, 900, Phaser.AUTO, 'game-mainpage', { preload: preload, create: create, update: update, render: render });
+
+// Initializing game =======================================================================
 
 function preload() {
 
@@ -14,33 +16,12 @@ var cursors;
 function create() {
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
-
-// CHARACTER
-  character = game.add.sprite(game.world.centerX-30, game.world.centerY-30, 'farmer');
-  character.scale.setTo(0.25, 0.25);
-  character.anchor.setTo(0.5, 0.5);
-  game.physics.enable(character, Phaser.Physics.ARCADE);
-  character.body.collideWorldBounds = true;
-  character.body.bounce.set(0.3);
-
-
-// BALL
-  ball = game.add.sprite(0, 0, 'flyer');
-  ball.scale.setTo(0.02, 0.02);
-  game.physics.enable(ball, Phaser.Physics.ARCADE);
-
-  ball.body.collideWorldBounds = true;
-  ball.body.bounce.set(1.1);
-  ball.body.velocity.setTo(400,400);
-
-// RANDOM
+  
+  createPlayer();
+  createBall();
 
   cursors = game.input.keyboard.createCursorKeys();
-}
 
-function createCharacter() {
-
-    character.create('farmer');
 }
 
 function update() {
@@ -51,8 +32,41 @@ function update() {
     else if (cursors.right.isDown) { character.body.velocity.x += 8; } 
     if (cursors.up.isDown) { character.body.velocity.y -= 8; }
     else if (cursors.down.isDown) { character.body.velocity.y += 8; }
+
+}
+
+function render() {
+
+    game.debug.text('Elapsed seconds: ' + this.game.time.totalElapsedSeconds(), 32, 32);
+
+}
+
+// Game methods ============================================================================
+
+function createPlayer() {
+
+  character = game.add.sprite(game.world.centerX-30, game.world.centerY-30, 'farmer');
+  character.scale.setTo(0.25, 0.25);
+  character.anchor.setTo(0.5, 0.5);
+  game.physics.enable(character, Phaser.Physics.ARCADE);
+  character.body.collideWorldBounds = true;
+  character.body.bounce.set(0.3);
+
+}
+
+function createBall() {
+
+  ball = game.add.sprite(0, 0, 'flyer');
+  ball.scale.setTo(0.02, 0.02);
+  game.physics.enable(ball, Phaser.Physics.ARCADE);
+  ball.body.collideWorldBounds = true;
+  ball.body.bounce.set(1.1);
+  ball.body.velocity.setTo(400,400);
+
 }
 
 function destroySprite() {
+
   character.kill();
+
 }
