8394321717629c94c4701302c94674cd41835ef6
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-13 11:13:06 +0000

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var server = require('http').createServer(app);
-var port = 9999;
 var expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
@@ -10,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 3001))
+app.set('port', (process.env.PORT || 9999))
 
 app.get('/', function(request, response){
   response.render('index');
@@ -25,8 +24,8 @@ app.get('/game', function(request, response){
   console.log(request.body.username);
 });
 
-server.listen(port, function(){
-  console.log('Server running at ' + port);
+server.listen(app.get('port'), function(){
+  console.log('Server running at ' + app.get('port'));
 });
 
 module.exports = server;
