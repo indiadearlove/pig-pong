0d611ed8192ed0cc2a3d6b3c8ab64ac52add599a
India Dearlove <india_dearlove@yahoo.co.uk>
2015-02-13 15:03:08 +0000

    changed sound for heroku

--- public/js/game.js
+++ public/js/game.js
@@ -88,13 +88,6 @@ function createPlayer() {
 
 }
 
-function audio() {
-
-  ex_sound = game.add.audio('ex_sound');
-  ex_sound.play();
-
-}
-
 function createBall() {
 
   ball = group.create(game.world.randomX, game.world.randomY, 'flyer', 1);
@@ -119,7 +112,6 @@ function destroySprite() {
   var explosionAnimation = explosion.getFirstExists(false);
   explosionAnimation.reset(character.x, character.y);
   explosionAnimation.play('explosion', 30, false, true);
-  audio();
 
   //highscore
 
